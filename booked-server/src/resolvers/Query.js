cda9f46a36d89f6664369c675e5640e3ea98784a
DJ <dtaylor88@gmail.com>
2021-11-25 18:31:02 -0800

    add filter to query

--- booked-server/src/resolvers/Query.js
+++ booked-server/src/resolvers/Query.js
@@ -1,6 +1,19 @@
-function auditions(parent, args, context) {
-    return context.prisma.audition.findMany()
-}
+async function auditions(parent, args, context, info) {
+    const where = args.filter
+      ? {
+        OR: [
+          { description: { contains: args.filter } },
+          { location: { contains: args.filter } },
+        ],
+      }
+      : {}
+  
+    const links = await context.prisma.audition.findMany({
+      where,
+    })
+  
+    return links
+  }
 
 module.exports = {
     auditions
